6bc6d8329b69345baab7146cfae1c9a3c07e357b
Reinaldo Martins <56726720+lbmle@users.noreply.github.com>
2023-09-03 15:01:22 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-let podeJogar = false
-let segundos = 0
-let jogada = 0
-let acertos = 0
-let startTimer
-let tempoFim = 10 //let contarAte
-let textoTempo = '*'
-
 function onLoad() {
     const dependencias = {
         tela: Tela,
