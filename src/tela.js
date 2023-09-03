634cb583506b9b294dc536b5e1fad177921ab41b
Reinaldo Martins <56726720+lbmle@users.noreply.github.com>
2023-09-03 15:03:33 -0300

    Update tela.js

--- src/tela.js
+++ src/tela.js
@@ -1,16 +1,24 @@
 const ID_BOTAO_JOGAR = "jogar"
-const ID_MENSAGEM = "mensagem"
+const ID_CONTADOR = "contador"
 const ID_FIM = "fim"
 const ID_CONTEUDO = "conteudo"
-const ID_PREPARANDO = "preparando"
-//const CLASSE_INVISIVEL = "invisible"
-const ID_PREPARADOR = "preparador"
-
-
+const ID_MENSAGEM = "mensagem"
+const CLASSE_INATIVO = "transparente"
+let startTimer
+let segundos
 
 class Tela {
 
+    static atualizarIcones(itens) {
+        const codigoHtml = Tela.gerarStringHTMLPelaImagem(itens)
+        Tela.alterarConteudoHTML(codigoHtml)
+    }
+
+    static gerarStringHTMLPelaImagem(data) {
+        return data.map(Tela.obterCodigoHtml).join('')
+    }
     static obterCodigoHtml(item) {
+
         return `   
             <span   onclick="window.verificarClique('${item.id}', '${item.nome}')">
                 <img name="${item.nome}" src="${item.img}" class="icone" alt="${item.nome}" />
@@ -23,83 +31,53 @@ class Tela {
         conteudo.innerHTML = codigoHtml
     }
 
-    static gerarStringHTMLPelaImagem(data) {
-        return data.map(Tela.obterCodigoHtml).join('')
-    }
-
-    static exibirMensagem() {
-        const elemento = document.getElementById(ID_MENSAGEM)
-        elemento.innerText = textoTempo
-    }
-
-    static iniciarPreparador(str, tm) {
+    static mostrarContagemRegressiva(str, tm) {
+        document.getElementById(ID_BOTAO_JOGAR).classList.add(CLASSE_INATIVO)
         let tempoInicio = tm
         const identificadorNoTexto = "$$contagem"
         const textoPadrao = `${str} ${identificadorNoTexto} segundos...`
-        const elementoPreparador = document.getElementById(ID_PREPARADOR)
-        const atualizarTexto = _ => { (elementoPreparador.innerHTML = textoPadrao.replace(identificadorNoTexto, tempoInicio--)) }
+        const elementoMensagem = document.getElementById(ID_MENSAGEM)
+        const atualizarTexto = _ => { (elementoMensagem.innerHTML = textoPadrao.replace(identificadorNoTexto, tempoInicio--)) }
         atualizarTexto()
         const idIntervalo = setInterval(atualizarTexto, 1000);
         return idIntervalo
     }
 
-    static mostrarFim(str) {
-        document.getElementById(ID_PREPARADOR).innerHTML = str
-    }
-
     static iniciarContador() {
-        const elementoContador = document.getElementById(ID_MENSAGEM)
+        const elementoContador = document.getElementById(ID_CONTADOR)
         const addSecond = _ => (elementoContador.innerHTML = segundos++)
         addSecond()
         startTimer = setInterval(addSecond, 1000);
-        textoTempo = `${segundos}`
     }
 
-    static mudarFim() {
+    static mudarFim(tm) {
         const elementoFim = document.getElementById(ID_FIM)
-        elementoFim.innerHTML = tempoFim
+        elementoFim.innerHTML = tm
     }
 
-    static pararContador() {
+    static pararContador(str) {
         clearInterval(startTimer)
-        textoTempo = '0'
-        podeJogar = false
-        document.getElementById(ID_PREPARADOR).innerHTML = "TEMPO ESGOTADO"
+        document.getElementById(ID_MENSAGEM).innerHTML = str
+        document.getElementById(ID_BOTAO_JOGAR).classList.remove(CLASSE_INATIVO)
     }
 
-    static limparPreparador(idContador) {
+    static zerarContador() {
+        const elementoContador = document.getElementById(ID_CONTADOR)
+        elementoContador.innerHTML = 0
+    }
+
+    static limparContagemRegressiva(idContador) {
         clearInterval(idContador)
-        document.getElementById(ID_PREPARADOR).innerHTML = "________ Tempo ___ Limite"
-    }
-
-    static exibirMensagem() { //mostrar = true) {
-        const elementoContador = document.getElementById(ID_MENSAGEM)
-        elementoContador.innerHTML = tempoFim
-        //this.exibirMensagem(true)
-        const prepara = document.getElementById(ID_PREPARANDO)
-        //if (mostrar) {
-        //      prepara.classList.remove(CLASSE_INVISIVEL)
-        //     return
-        //  }
-        //  prepara.classList.add(CLASSE_INVISIVEL)
-    }
-
-    static atualizarIcones(itens) {
-        const codigoHtml = Tela.gerarStringHTMLPelaImagem(itens)
-        Tela.alterarConteudoHTML(codigoHtml)
-    }
-
-    static exibirIcones(nome, img) {
-        const elements = document.getElementsByName(nome)
-        elements.forEach(item => (item.src = img))
+        document.getElementById(ID_MENSAGEM).innerHTML = "________ Tempo ___ Limite"
     }
 
     static configurarBotaoJogar(funcaoOnclick) {
         const btnJogar = document.getElementById(ID_BOTAO_JOGAR)
         btnJogar.onclick = funcaoOnclick
+
     }
 
-    static configurarClickVerificarClique(funcaoOnclick) {
+    static configurarClique(funcaoOnclick) {
         window.verificarClique = funcaoOnclick
     }
 }
