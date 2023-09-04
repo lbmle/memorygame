53523d049140e9e79cae01048cbdc6e41d237f4b
Reinaldo Martins <56726720+lbmle@users.noreply.github.com>
2023-09-03 22:46:38 -0300

    icones iniciais aqui

--- src/tela.js
+++ src/tela.js
@@ -6,10 +6,26 @@ const ID_MENSAGEM = "mensagem"
 const CLASSE_INATIVO = "transparente"
 let startTimer
 let segundos
+let pasta = 1
+let inicio = 0
+let iconesIniciais = []
 
 class Tela {
 
-    static atualizarIcones(itens) {
+    static atualizarIcones(path, itens) {
+        pasta = path
+        iconesIniciais = [
+            { img: `./icones/${pasta}/1.gif`, nome: '1' },
+            { img: `./icones/${pasta}/2.gif`, nome: '2' },
+            { img: `./icones/${pasta}/3.gif`, nome: '3' },
+            { img: `./icones/${pasta}/4.gif`, nome: '4' },
+            { img: `./icones/${pasta}/5.gif`, nome: '5' },
+            { img: `./icones/${pasta}/6.gif`, nome: '6' },
+            { img: `./icones/${pasta}/7.gif`, nome: '7' },
+            { img: `./icones/${pasta}/8.gif`, nome: '8' },
+            { img: `./icones/${pasta}/9.gif`, nome: '9' },
+            { img: `./icones/${pasta}/10.gif`, nome: '10' },
+        ]
         const codigoHtml = Tela.gerarStringHTMLPelaImagem(itens)
         Tela.alterarConteudoHTML(codigoHtml)
     }
@@ -27,8 +43,10 @@ class Tela {
     }
 
     static alterarConteudoHTML(codigoHtml) {
-        const conteudo = document.getElementById(ID_CONTEUDO)
-        conteudo.innerHTML = codigoHtml
+        if (inicio) {
+            const conteudo = document.getElementById(ID_CONTEUDO)
+            conteudo.innerHTML = codigoHtml
+        }
     }
 
     static mostrarContagemRegressiva(str, tm) {
@@ -59,6 +77,12 @@ class Tela {
         clearInterval(startTimer)
         document.getElementById(ID_MENSAGEM).innerHTML = str
         document.getElementById(ID_BOTAO_JOGAR).classList.remove(CLASSE_INATIVO)
+        if (str === "FIM") {
+            const conteudo = document.getElementById(ID_CONTEUDO)
+            conteudo.innerHTML = `
+            <img src = "./icones/fim.gif" width="340" height="240">
+            `
+        }
     }
 
     static zerarContador() {
